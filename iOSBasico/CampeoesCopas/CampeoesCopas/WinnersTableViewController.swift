ff7c3f1fa54653bef392624e916418f30434c176
Douglas Frari <douglas.frari@gmail.com>
2020-03-14 09:40:52 -0300

    Subindo versao com tarefa #58 feita

--- iOSBasico/CampeoesCopas/CampeoesCopas/WinnersTableViewController.swift
+++ iOSBasico/CampeoesCopas/CampeoesCopas/WinnersTableViewController.swift
@@ -88,14 +88,22 @@ class WinnersTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        
+        // usamos essa forma para previnir quando tivermos mais de uma Segue no
+        // Storyboard.
+        if segue.identifier == "showWinner" {
+            
+            let vc = segue.destination as! WorldCupViewController
+            let worldCup = worldCups[tableView.indexPathForSelectedRow!.row]
+            vc.worldCup = worldCup
+        }
+        
     }
-    */
+    
 
 }
--- iOSBasico/CampeoesCopas/CampeoesCopas/WorldCupViewController.swift
+++ iOSBasico/CampeoesCopas/CampeoesCopas/WorldCupViewController.swift
@@ -10,21 +10,14 @@ import UIKit
 
 class WorldCupViewController: UIViewController {
 
+    var worldCup: WorldCup!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        self.title = worldCup.winner
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
+    
 }
