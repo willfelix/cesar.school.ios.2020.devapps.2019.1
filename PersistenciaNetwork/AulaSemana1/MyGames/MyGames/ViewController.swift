977121e0c2c5f881aa90a096e19701ec4c8506bc
Douglas Del Frari <douglas.frari@gmail.com>
2020-05-16 08:38:42 -0300

    Versão da aula (iOS – Network e persistência de dados)  - parte 1

---
+++ PersistenciaNetwork/AulaSemana1/MyGames/MyGames/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyGames
+//
+//  Created by Douglas Frari on 15/05/20.
+//  Copyright © 2020 Douglas Frari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
