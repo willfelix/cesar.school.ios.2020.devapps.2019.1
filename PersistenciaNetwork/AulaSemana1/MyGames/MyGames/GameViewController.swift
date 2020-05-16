530838a2d7f65248a113d7bcc633b9c85d6863c3
Douglas Del Frari <douglas.frari@gmail.com>
2020-05-16 12:57:54 -0300

    Versão da parte 2 (aula de sabado pela manha)

---
+++ PersistenciaNetwork/AulaSemana1/MyGames/MyGames/GameViewController.swift
@@ -0,0 +1,37 @@
+//
+//  GameViewController.swift
+//  MyGames
+//
+//  Created by Douglas Frari on 16/05/20.
+//  Copyright © 2020 Douglas Frari. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    
+    @IBOutlet weak var lbTitle: UILabel!
+    @IBOutlet weak var lbConsole: UILabel!
+    @IBOutlet weak var lbReleaseDate: UILabel!
+    @IBOutlet weak var ivCover: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
